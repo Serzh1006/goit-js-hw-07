251867f20aeea82958894206045b271d3d13cb24
Sergey Kravchenko <skravchenko223@gmail.com>
2024-11-18 22:33:06 +0100

    task-5 is done.

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChange = document.querySelector('.change-color');
+const colorText = document.querySelector('.color');
+
+btnChange.addEventListener('click', e => {
+  const getColor = getRandomHexColor();
+  document.body.style.backgroundColor = getColor;
+  colorText.textContent = getColor;
+});
