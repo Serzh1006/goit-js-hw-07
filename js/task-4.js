1c8ce48e694688f513c1765b2b4a0b2b46d7881c
Sergey Kravchenko <skravchenko223@gmail.com>
2024-11-18 22:22:58 +0100

    task-4 is done.

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (
+    form.elements[0].value.trim() === '' ||
+    form.elements[1].value.trim() === ''
+  ) {
+    alert('All form fields must be filled in');
+  } else {
+    const emailName = e.currentTarget.elements.email.name;
+    const passwordName = e.currentTarget.elements.password.name;
+    const emailValue = e.currentTarget.elements.email.value.trim();
+    const passwordValue = e.currentTarget.elements.password.value.trim();
+    const dataForm = {
+      [emailName]: emailValue,
+      [passwordName]: passwordValue,
+    };
+    console.log(dataForm);
+    e.currentTarget.reset();
+  }
+});
