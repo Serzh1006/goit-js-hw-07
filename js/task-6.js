3763f43721aacebeb217229abf69c2b708b5ff21
Sergey Kravchenko <skravchenko223@gmail.com>
2024-11-18 23:23:15 +0100

    task-6 is done.

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const textInput = document.querySelector('#controls');
+const boxes = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const counter = textInput.firstElementChild.value;
+  const collections = [];
+  let increment = 30;
+
+  if (counter >= 1 && counter <= 100) {
+    for (let i = 0; i < counter; i += 1) {
+      const newEl = document.createElement('div');
+      newEl.style.width = `${increment}px`;
+      newEl.style.height = `${increment}px`;
+      newEl.style.backgroundColor = getRandomHexColor();
+      collections.push(newEl);
+      increment += 10;
+    }
+    boxes.innerHTML = '';
+    boxes.append(...collections);
+    textInput.firstElementChild.value = '';
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
